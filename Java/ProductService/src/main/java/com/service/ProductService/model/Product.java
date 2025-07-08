60a26eaf8d52d4b5844f0d045d59ae79f24f252d
Manjunatha <k.manjunatha1518@gmail.com>
2025-07-08 23:00:04 +0530

    ProductService

---
+++ Java/ProductService/src/main/java/com/service/ProductService/model/Product.java
@@ -0,0 +1,34 @@
+package com.service.ProductService.model;
+
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Product {
+    Integer id;
+    String name;
+
+    public Product(){
+
+    }
+    public Product(Integer id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
