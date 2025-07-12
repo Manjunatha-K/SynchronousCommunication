ebabcd200d26c67f2084bf8fa4303a4cc52b0c24
Manjunatha <k.manjunatha1518@gmail.com>
2025-07-12 16:54:52 +0530

    RestClient

--- RestClient/OrderService/src/main/java/com/service/OrderService/Config/OrderConfig.java
+++ RestClient/OrderService/src/main/java/com/service/OrderService/Config/OrderConfig.java
@@ -1,7 +1,15 @@
 package com.service.OrderService.Config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.client.RestClient;
 
 @Configuration
 public class OrderConfig {
+
+    @Bean
+    public RestClient getRestClient() {
+        return RestClient.create();
+    }
 }
--- RestClient/OrderService/src/main/java/com/service/OrderService/Controller/OrderController.java
+++ RestClient/OrderService/src/main/java/com/service/OrderService/Controller/OrderController.java
@@ -1,11 +1,14 @@
 package com.service.OrderService.Controller;
 
 import com.service.OrderService.Model.Order;
+import com.service.OrderService.Model.Product;
 import com.service.OrderService.Service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestClient;
 
 import java.util.List;
 
@@ -15,9 +18,14 @@ public class OrderController {
     @Autowired
     OrderService orderService;
 
+    @Autowired
+    RestClient restClient;
+
     @GetMapping("/order")
     public ResponseEntity<List<Order>> getAllOrders(){
-
+ResponseEntity<List> products = restClient.get().uri("http://localhost:8081/Product/product")
+        .retrieve().toEntity(List.class);
+products.getBody().stream().forEach(a-> System.out.println(a));
         return new ResponseEntity<>(orderService.getAllOrders(), HttpStatus.OK);
     }
     @PostMapping("/order")
@@ -34,5 +42,12 @@ public class OrderController {
     public ResponseEntity<List<Order>> deleteOrder(@RequestParam(name = "id") int id){
 
         return new ResponseEntity<>(orderService.deleteOrderService(id), HttpStatus.FOUND);
+    }
+
+    @PostMapping("/product")
+    public ResponseEntity<Product> addOrder(@RequestBody Product product){
+
+        return restClient.post().uri("http://localhost:8081/Product/product")
+                .contentType(MediaType.APPLICATION_JSON).body(product).retrieve().toEntity(Product.class);
     }
 }
